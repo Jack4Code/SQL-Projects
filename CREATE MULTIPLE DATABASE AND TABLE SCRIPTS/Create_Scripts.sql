165a6260d74ba6db0704fb8a16f91b8e593f87e7
jack.giannini@gmail.com <jack.giannini@gmail.com>
2018-05-31 06:47:08 -0400

    commit for merging

--- CREATE MULTIPLE DATABASE AND TABLE SCRIPTS/Create_Scripts.sql
+++ CREATE MULTIPLE DATABASE AND TABLE SCRIPTS/Create_Scripts.sql
@@ -4,8 +4,8 @@
 
 DECLARE @databaseName varchar(20) = 'testDatabase'; --decide the database prefix name
 DECLARE @database int = 1; --first one
-DECLARE @dbQTY int = 5; --decide how many you want
-DECLARE @createOrDrop varchar(6) = 'create' -- 'create' them or 'drop' them.
+DECLARE @dbQTY int = 9; --decide how many you want
+DECLARE @createOrDrop varchar(6) = 'drop' -- 'create' them or 'drop' them.
 PRINT(UPPER(@createOrDrop) + ' script is being run.')
 if(UPPER(@createOrDrop) = 'CREATE')
 BEGIN
@@ -36,7 +36,24 @@ ELSE
 BEGIN
 	PRINT(CAST(@dbQTY as varchar(4)) + ' databases were droped.')
 END
+
 --NOW DEPLOY TABLES ACROSS HOWEVER MANY DATABASES THERE ARE
+--IF(UPPER(@createOrDrop) = 'CREATE')
+--DECLARE @databaseNameWithPercent varchar(20) = @databaseName + '%'
+--BEGIN
+--	exec sp_msforeachdb 
+--	'USE [?];
+--	IF db_name() like ' + @databaseNameWithPercent + ' begin 
+--    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name=''AccountHolder'' AND xtype=''U'')
+--	BEGIN
+--	Print(''Creating AccountHolder in database '' + db_name())
+--	CREATE table dbo.AccountHolder (Id int IDENTITY(1,1) NOT NULL,
+--	[Date] datetime Not Null,
+--	TransDetail nvarchar(max) not null,
+--	credit bit not null,
+--	Balance money not null,
+--	Primary Key (Id)) END END'
+--END
 
 
 
